64cf8999f72ec0aa2cfec1e991dab134aaeeeb3a
wirekang <kanghyunsoo1211@gmail.com>
2020-06-06 16:05:44 +0900

    Use 'readline' to handle inputs

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,24 @@
 import fs = require("fs")
+import readline = require("readline")
 
-console.log("hello")
+initReadline(onLine)
+
+function onLine(line: string) {
+    line = line.trim()
+    const args = line.split(" ")
+    switch (args[0]) {
+        case "exit": process.exit()
+            break
+        case "help": console.log("exit")
+
+    }
+}
+
+function initReadline(listener: (line: string) => void) {
+    readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    }).on("line", listener).on("close", () => {
+        process.exit()
+    })
+}
