13ebd72fbd6958f4ceb1d8acc044e1dcd8eaaaa7
wirekang <kanghyunsoo1211@gmail.com>
2020-06-07 22:27:29 +0900

    Fix main to handle config.json missing

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,26 +3,30 @@ import readline = require("readline")
 import Server from "./Server"
 import FileWatcher from "./FileWatcher"
 
+var server: Server
 const config = initConfig()
 initReadline(onLine)
 
-const server = new Server(config.port)
-new FileWatcher(config.file, onFileChange)
+if (config !== null) {
+    server = new Server(config.port)
+    new FileWatcher(config.file, onFileChange)
+}
 
 function onFileChange() {
     server.transferFile(config.file)
 }
 
 function onLine(line: string) {
+    if (config === null) {
+        console.log(">>>Please modify config.json and restart<<<")
+        return
+    }
     try {
         line = line.trim()
         const args = line.split(" ")
         switch (args[0]) {
             case "send":
                 server.transferFile(config.file)
-                break
-            case "event":
-                server.sendEmptyEvent(args[1])
                 break
             case "exit": process.exit()
             case "help": console.log("send exit")
@@ -33,10 +37,19 @@ function onLine(line: string) {
 }
 
 function initConfig(): any {
-    console.log("Reading config.json...")
-    const config = JSON.parse(fs.readFileSync("config.json").toString())
-    console.log(config)
-    return config
+    if (fs.existsSync("config.json")) {
+        console.log("Reading config.json...")
+        const config = JSON.parse(fs.readFileSync("config.json").toString())
+        console.log(config)
+        return config
+    } else {
+        console.log("Config file not found. Create a new one.")
+        const config = { "file": "C:/example/debug/app-debug.apk", "port": 5123 }
+        fs.writeFileSync("config.json", JSON.stringify(config))
+        console.log(">>>Please modify config.json and restart<<<")
+        return null
+    }
+
 }
 
 function initReadline(listener: (line: string) => void) {
