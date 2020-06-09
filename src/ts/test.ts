04a5112523e9507ab8e2f7d66c42e1ee417845a6
wirekang <kanghyunsoo1211@gmail.com>
2020-06-09 22:05:16 +0900

    Remove unnecessary files

--- src/ts/test.ts
+++
@@ -1,9 +0,0 @@
-import BufferReader from "./BufferReader"
-
-const buffer = Buffer.alloc(12)
-buffer.writeUInt16BE(10)
-buffer.write("1234567890", 2)
-
-const reader = new BufferReader(buffer)
-console.log("test")
-console.log(`>>${reader.readString()}<<`)
